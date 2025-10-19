0ecb6d867a2cb2c0915a1844cc0d26e77565eb9c
LeTanSang <xuandongtk@gmail.com>
2025-10-19 15:45:12 +0700

    git branch

--- src/test/java/interfaces/elements/CheckBoxPageInterface.java
+++ src/test/java/interfaces/elements/CheckBoxPageInterface.java
@@ -4,12 +4,6 @@ import org.openqa.selenium.By;
 
 public interface CheckBoxPageInterface {
     String EXPAND_ALL_BUTTON = "//button[@title='Expand all']";
-
-    // Locator động: truyền tên node (Home / Desktop / Documents / ...)
     String NODE_BY_TEXT      = "//span[@class='rct-title' and normalize-space(text())='%s']";
-
-    // Khu vực kết quả “You have selected”
     String RESULT_BOX        = "//div[@id='result']";
-    // Các item màu xanh trong kết quả
-    String RESULT_ITEMS      = "//div[@id='result']//span[@class='text-success']";
 }
